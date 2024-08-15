39952cf0a37da5c6f223cc3dca0c91748dbbf0fe
Ahmad Alfoqhaa <Ahmadalfouqaha@hotmail.com>
2024-08-15 10:24:37 +0200

    Removed unnecessary information import from the API (sources array from weather informaition)

--- withCityAPI.go
+++ withCityAPI.go
@@ -31,23 +31,9 @@ type WeatherByHour struct {
 	Icon				string   `json:"icon"`
 }
 
-type Sources struct {
-	Id				int       `json:"id"`
-	DwdStationId			string    `json:"dwd_station_id"`
-	ObservationType			string    `json:"observation_type"`
-	Lat				float64   `json:"lat"`
-	Lon				float64   `json:"lon"`
-	Height				float64   `json:"height"`
-	StationName			string    `json:"station_name"`
-	WmoStationId			string    `json:"wmo_station_id"`
-	FirstRecord			time.Time `json:"first_record"`
-	LastRecord			time.Time `json:"last_record"`
-	Distance			float64   `json:"distance"`
-}
 
 type WeatherbyDay struct {
 	WeatherByHours	[]WeatherByHour		`json:"weather"`
-	Source		[]Sources		`json:"sources"`
 }
 
 type Cityinfo struct {
@@ -75,14 +61,9 @@ case day < 1 || day > 31:
 fmt.Println("check day input")
 return
 
-//case lat < -90 || lat > 90:
-//fmt.Println("check latitude input")
-
-//case lon < -180 || lat > 180:
-//fmt.Println("check longitude input")
-
 default:
 SendRequest(year, month, day, hour, city)
+
 }
 }
 
@@ -104,7 +85,6 @@ func GetLatLong (city string){
 	}
 	
 	err = json.Unmarshal(body, &citynumbers)
-
 }
 
 func SendRequest(year int, month int, day int, hour int, city string) {
@@ -137,7 +117,7 @@ func SendRequest(year int, month int, day int, hour int, city string) {
 
 func main() {
 
-	var city string = "berlin"	// city name
+	var city string = "Muenchen"	// city name
 	var year int	= 2024		// year
 	var month int 	= 8		// month
 	var day int 	= 14		// day
