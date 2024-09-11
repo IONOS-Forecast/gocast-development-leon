f65caff461b46eabd93ed38eca05a757d765344d
Leon Zuelsdorf <leonzuelsdorf@gmail.com>
2024-09-11 15:47:31 +0200

    Changed: convert.sh | changed converting function

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -2,17 +2,19 @@
 
 PROJECT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}")/..)
 
-cities=("berlin" "hamburg" "munich")
+cities=("berlin")
 
 echo "removing existing weather records"
 for c in "${cities[@]}"; do
   rm -f "resources/pg/data/$c".csv
 done;
 
+mkdir -p "../resources/pg/data"
+
 echo "generating CSV files to COPY"
 for c in "${cities[@]}"; do
-  for i in {1..7}; do
-    jq --arg city "$c" -r '.weather | del(.[].fallback_source_ids) | .[].city+=$city | map([.[]]) | .[:-1] | .[] | @csv' "resources/weather_records/$c"_0"$i"-09-orig.json >> "resources/pg/data/$c".csv
+  for i in {0..7}; do
+    jq --arg city "$c" -r '.weather | del(.[].fallback_source_ids) | .[].city+=$city | map([.[]]) | .[:-1] | .[] | @csv' "resources/weather_records/$c"_"$i"-orig.json >> "resources/pg/data/$c".csv
   done;
   printf "%s" "$(< resources/pg/data/$c.csv)" > "resources/pg/data/$c".csv
 done;
