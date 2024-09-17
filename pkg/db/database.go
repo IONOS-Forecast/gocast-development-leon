58009c0d5d75f38cf757c099458311724c87487c
Leon Zuelsdorf <leonzuelsdorf@gmail.com>
2024-09-17 14:04:27 +0200

    Database Insert Fix

--- pkg/db/database.go
+++ pkg/db/database.go
@@ -1,10 +1,7 @@
 package db
 
 import (
-	"encoding/csv"
 	"fmt"
-	"os"
-	"strconv"
 	"strings"
 	"time"
 
@@ -51,14 +48,14 @@ func connectToDatabase(user, password, database, address string) pg.DB {
 	return *db
 }
 
-func (f postgresDB) QueryDayDatabase(city string, date string) ([]model.HourWeatherRecord, error) {
-	var res []model.HourWeatherRecord
+func (f postgresDB) QueryDayDatabase(city string, date string) ([]model.Weather_record, error) {
+	var res []model.Weather_record
 	year, month, day, err := utils.SplitDate(date)
 	if err != nil {
-		return []model.HourWeatherRecord{}, fmt.Errorf("%v\nERROR: Can't query Database because of date failure!", err)
+		return []model.Weather_record{}, fmt.Errorf("%v\nERROR: Can't query Database because of date failure!", err)
 	}
 	if city == "" {
-		return []model.HourWeatherRecord{}, fmt.Errorf("ERROR: Can't query Database because city %v isn't set!", city)
+		return []model.Weather_record{}, fmt.Errorf("ERROR: Can't query Database because city %v isn't set!", city)
 	}
 	query := fmt.Sprintf("timestamp::date='%v-%.2v-%.2v 00:00:00+00' AND city='%v'", year, month, day, city)
 	err = pgDB.Model().Table("weather_records").
@@ -115,15 +112,26 @@ func (f postgresDB) GetWeatherRecord(city, date string) (model.WeatherRecord, er
 			if err != nil {
 				return model.WeatherRecord{}, fmt.Errorf("ERROR: Requesting weather threw an error!\nERROR: %v", err)
 			}
-			err = utils.SaveFutureWeatherInFile(city, date)
+			_, err := utils.SaveFutureWeatherInFile(city, date)
 			if err != nil {
 				return model.WeatherRecord{}, fmt.Errorf("ERROR: Saving future weather threw an error!\nERROR: %v", err)
 			}
 		}
-		err := f.InsertCityWeatherRecordsToTable(city)
+		records, err := utils.GetAllWeatherRecords(city, date)
+		for i := 0; i < len(records); i++ {
+			err := f.InsertCityWeatherRecordsToTable(records[i])
+			fmt.Println(records[i].Hours[0].City)
+			if err != nil {
+				return model.WeatherRecord{}, err
+			}
+		}
+		year, month, day, err := utils.SplitDate(date)
 		if err != nil {
 			return model.WeatherRecord{}, err
 		}
+		fmt.Println(date)
+		utils.SetDate(year, month, day)
+		fmt.Println(date)
 	}
 	if !utils.PathExists(fmt.Sprintf("resources/weather_records/%v_0-orig.json", city)) && dataExists {
 		fmt.Println("INFO: Weather records don't exist! Getting weather records from Database.")
@@ -141,63 +149,38 @@ func (f postgresDB) GetWeatherRecord(city, date string) (model.WeatherRecord, er
 
 func (f postgresDB) getHourWeatherRecord(city, date string) (model.WeatherRecord, error) {
 	city = strings.ToLower(city)
-	records, err := f.QueryDayDatabase(city, date)
+	_, err := f.QueryDayDatabase(city, date)
 	if err != nil {
 		return model.WeatherRecord{}, err
 	}
 	var today model.WeatherRecord
-	today.Hours = records
+	//today.Hours = records
 	return today, nil
 }
 
-func (f postgresDB) InsertCityWeatherRecordsToTable(city string) error {
-	path := fmt.Sprintf("resources/pg/data/%v.csv", city)
-	if !utils.PathExists(path) {
-		return fmt.Errorf("ERROR: Path \"%v\" ", path)
-	}
-	utils.ConvertWeatherRecords()
-	file, err := os.Open(path)
-	if err != nil {
-		return fmt.Errorf("ERROR: Couldn't open file (%v)", path)
-	}
-	defer file.Close()
-	csvReader := csv.NewReader(file)
-	records, err := csvReader.ReadAll()
-	if err != nil {
-		return fmt.Errorf("ERROR: Couldn't read file %v.csv\nERROR: %v", city, err)
-	}
-	_, err = pgDB.Exec(`CREATE TEMPORARY TABLE temp_weather_records (id int NOT NULL, timestamp TIMESTAMP, source_id INT, precipitation FLOAT, pressure_msl FLOAT, sunshine FLOAT, temperature FLOAT, wind_direction INT, wind_speed FLOAT, cloud_cover FLOAT, dew_point FLOAT, relative_humidity FLOAT, visibility FLOAT, wind_gust_direction INT, wind_gust_speed FLOAT, condition VARCHAR(100), precipitation_probability FLOAT, precipitation_probability_6h FLOAT, solar FLOAT, icon VARCHAR(100), city VARCHAR(100), PRIMARY KEY(ID));`)
-	if err != nil {
-		return fmt.Errorf("ERROR: Couldn't create temporary table temp_weather_records\nERROR: %v", err)
-	}
-	var csvString []string
+func (f postgresDB) InsertCityWeatherRecordsToTable(record model.WeatherRecord) error {
 	var count int
-	_, err = pgDB.Query(pg.Scan(&count), "SELECT COUNT(*) FROM weather_records")
+	_, err := pgDB.Query(pg.Scan(&count), "SELECT COUNT(id) FROM weather_records")
 	if err != nil {
 		return fmt.Errorf("ERROR: Count failed!\nERROR: %v", err)
 	}
-	for _, inner := range records {
-		inner = append([]string{strconv.Itoa(count + 1)}, inner...)
-		csvString = append(csvString, strings.Join(inner, ","))
+	fmt.Println(count)
+	city := utils.GetCityName()
+	if strings.Contains(city, "ü") {
+		city = "munich"
+	}
+	for i := 0; i < len(record.Hours)-1; i++ {
 		count++
+		record.Hours[i].ID = count
+		record.Hours[i].City = strings.ToLower(city)
+		_, err = pgDB.Model(&record.Hours[i]).Where("timestamp = ?", record.Hours[i].TimeStamp).Where("city = ?", record.Hours[i].City).
+			OnConflict("DO NOTHING").SelectOrInsert()
+		if err != nil {
+			return fmt.Errorf("failed insert: %v", err)
+		}
 	}
-	csvData := strings.Join(csvString, "\n")
-	reader := strings.NewReader(csvData)
-	_, err = pgDB.CopyFrom(reader, `COPY temp_weather_records FROM STDIN WITH CSV`)
-	if err != nil {
-		return fmt.Errorf("ERROR: Couldn't copy temp_weather_records\nERROR: %v", err)
-	}
-	_, err = pgDB.Exec("INSERT INTO weather_records\n" +
-		"SELECT * FROM temp_weather_records WHERE timestamp NOT IN (SELECT timestamp FROM weather_records)" +
-		" AND city NOT IN (SELECT city FROM weather_records)")
-	if err != nil {
-		return fmt.Errorf("ERROR: Couldn't insert temp_weather_records into weather_records\nERROR: %v", err)
-	}
-	_, err = pgDB.Exec("DROP TABLE temp_weather_records")
-	if err != nil {
-		return fmt.Errorf("failed to drop temp_weather_records: %v", err)
-	}
-	fmt.Println("Weather Data inserted into Table!")
+
+	//_, err = pgDB.Model(&record.Hours).Where("timestamp = ?", record.Hours[0].TimeStamp).Where("city = ?", city).OnConflict("DO NOTHING").SelectOrInsert()
 	return nil
 }
 
@@ -205,22 +188,47 @@ func (f postgresDB) Close() {
 	pgDB.Close()
 }
 
-/*
-func saveFutureWeather(city string, count string, record model.WeatherRecord) error {
-	data, err := json.MarshalIndent(record, "", "  ")
-	if err != nil {
-		return fmt.Errorf("ERROR: MarshalIndent threw an error!\nERROR: %v", err)
-	}
-
-	err = saveFile("resources/weather_records", strings.ToLower(city)+"_"+count+"-orig.json", data)
-	if err != nil {
-		return err
-	}
-	return nil
+/*path := fmt.Sprintf("resources/pg/data/%v.csv", city)
+if !utils.PathExists(path) {
+	return fmt.Errorf("ERROR: Path \"%v\" ", path)
+}
+utils.ConvertWeatherRecords()
+file, err := os.Open(path)
+if err != nil {
+	return fmt.Errorf("ERROR: Couldn't open file (%v)", path)
+}
+defer file.Close()
+csvReader := csv.NewReader(file)
+records, err := csvReader.ReadAll()
+if err != nil {
+	return fmt.Errorf("ERROR: Couldn't read file %v.csv\nERROR: %v", city, err)
+}
+_, err = pgDB.Exec(`CREATE TEMPORARY TABLE IF NOT EXISTS temp_weather_records (id INT NOT NULL, timestamp TIMESTAMP, source_id INT, precipitation FLOAT, pressure_msl FLOAT, sunshine FLOAT, temperature FLOAT, wind_direction INT, wind_speed FLOAT, cloud_cover FLOAT, dew_point FLOAT, relative_humidity FLOAT, visibility FLOAT, wind_gust_direction INT, wind_gust_speed FLOAT, condition VARCHAR(100), precipitation_probability FLOAT, precipitation_probability_6h FLOAT, solar FLOAT, icon VARCHAR(100), city VARCHAR(100), PRIMARY KEY(ID));`)
+if err != nil {
+	return fmt.Errorf("ERROR: Couldn't create temporary table temp_weather_records\nERROR: %v", err)
+}
+var csvString []string
+var count int
+_, err = pgDB.Query(pg.Scan(&count), "SELECT COUNT(*) FROM weather_records")
+if err != nil {
+	return fmt.Errorf("ERROR: Count failed!\nERROR: %v", err)
+}
+for _, inner := range records {
+	inner = append([]string{strconv.Itoa(count + 1)}, inner...)
+	csvString = append(csvString, strings.Join(inner, ","))
+	count++
+}
+csvData := strings.Join(csvString, "\n")
+reader := strings.NewReader(csvData)
+_, err = pgDB.CopyFrom(reader, `COPY temp_weather_records FROM STDIN WITH CSV`)
+if err != nil {
+	return fmt.Errorf("ERROR: Couldn't copy temp_weather_records\nERROR: %v", err)
+}
+_, err = pgDB.Exec("INSERT INTO weather_records SELECT * FROM temp_weather_records ON CONFLICT (id, timestamp, city) DO NOTHING")
+if err != nil {
+	return fmt.Errorf("failed to insert temp_weather_records into weather_records: %v", err)
+}
+_, err = pgDB.Exec("DROP TABLE temp_weather_records")
+if err != nil {
+	return fmt.Errorf("failed to drop temp_weather_records: %v", err)
 }*/
-
-/*
-TODO:
-- Make location.go
-- Remove all errors
-*/
