2d47552eb6bd366e34a5f581d904fb9ea58615db
Leon Zuelsdorf <leonzuelsdorf@gmail.com>
2024-10-08 16:12:49 +0200

    Implement part of testing and improve database interface

---
+++ pkg/metric/rest_test.go
@@ -0,0 +1,21 @@
+package metric_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/IONOS-Forecast/gocast-development-leon/Gocast/pkg/metric"
+)
+
+func TestGet(t *testing.T) {
+	req := httptest.NewRequest("GET", "http://localhost:8080/direct?date=2024-10-08&city=Berlin", nil)
+	rr := httptest.NewRecorder()
+	var h metric.Handler
+	h.Get(rr, req)
+	//handler := http.HandlerFunc(h.Get)
+	//handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
