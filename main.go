57fbf807570041422279675e3429f9828210185c
Leon Zuelsdorf <leonzuelsdorf@gmail.com>
2024-10-08 11:40:27 +0200

    Added Prometheus

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 
 	"github.com/IONOS-Forecast/gocast-development-leon/Gocast/pkg/db"
 	"github.com/IONOS-Forecast/gocast-development-leon/Gocast/pkg/metric"
+	"github.com/IONOS-Forecast/gocast-development-leon/Gocast/pkg/model"
 	"github.com/IONOS-Forecast/gocast-development-leon/Gocast/pkg/utils"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
@@ -18,6 +21,8 @@ var (
 )
 
 func main() {
+	reg := prometheus.NewRegistry()
+	m := NewMetrics(reg)
 	utils.MakeCities()
 	utils.DotEnvironment()
 	utils.SetWeatherAPIURL(utils.Options.WeatherAPIURL)
@@ -39,26 +44,29 @@ func main() {
 	if err != nil {
 		log.Print(err)
 	}
-	_, err = database.GetWeatherRecord(cityName, date)
+	record, err := database.GetWeatherRecord(cityName, date)
 	if err != nil {
 		log.Print(err)
 	}
+	registerMetrics(record, m)
 	cityName, err = database.SetLocationByCityName("München")
 	if err != nil {
 		log.Print(err)
 	}
-	_, err = database.GetWeatherRecord(cityName, date)
+	record, err = database.GetWeatherRecord(cityName, date)
 	if err != nil {
 		log.Print(err)
 	}
+	registerMetrics(record, m)
 	cityName, err = database.SetLocationByCityName("Hamburg")
 	if err != nil {
 		log.Print(err)
 	}
-	_, err = database.GetWeatherRecord(cityName, date)
+	record, err = database.GetWeatherRecord(cityName, date)
 	if err != nil {
 		log.Print(err)
 	}
+	registerMetrics(record, m)
 	/* TODO: ADD OR REMOVE LATER
 	Error Examples and MinutesRequest
 	fmt.Println(":----------------------------------------------------------------------------:")
@@ -79,9 +87,66 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	requestWeatherEvery(time.Duration(minutesRequest*int(time.Minute)), showWeather)*/
+	*/
+	promHandler := promhttp.HandlerFor(reg, promhttp.HandlerOpts{})
 	var handler metric.Handler
-	http.HandleFunc("/GET", handler.Get)
-	http.HandleFunc("/error", handler.Error)
-	http.ListenAndServe(":3333", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/GET", handler.Get)
+	mux.HandleFunc("/error", handler.Error)
+
+	pMux := http.NewServeMux()
+	pMux.Handle("/metrics", promHandler)
+	go func() {
+		log.Fatal(http.ListenAndServe(":8080", mux))
+	}()
+	go func() {
+		log.Fatal(http.ListenAndServe(":8081", pMux))
+	}()
+	select {}
+}
+
+type Metrics struct {
+	temperature *prometheus.GaugeVec
+	humidity    *prometheus.GaugeVec
+	windspeed   *prometheus.GaugeVec
+	pressure    *prometheus.GaugeVec
+}
+
+func NewMetrics(reg prometheus.Registerer) *Metrics {
+	m := &Metrics{
+		temperature: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "gocast",
+			Name:      "temperature",
+			Help:      "Temperature of each city",
+		}, []string{"location"}),
+		humidity: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "gocast",
+			Name:      "humidity",
+			Help:      "Humidity of each city",
+		}, []string{"location"}),
+		windspeed: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "gocast",
+			Name:      "wind_speed",
+			Help:      "Wind speed of each city",
+		}, []string{"location"}),
+		pressure: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "gocast",
+			Name:      "pressure",
+			Help:      "Pressure of each city",
+		}, []string{"location"}),
+	}
+	reg.MustRegister(m.temperature)
+	reg.MustRegister(m.humidity)
+	reg.MustRegister(m.windspeed)
+	reg.MustRegister(m.pressure)
+	return m
+}
+
+func registerMetrics(record model.WeatherRecord, m *Metrics) {
+	if len(record.Hours) != 0 {
+		m.temperature.With(prometheus.Labels{"location": record.Hours[0].City}).Set(record.Hours[0].Temperature)
+		m.humidity.With(prometheus.Labels{"location": record.Hours[0].City}).Set(float64(record.Hours[0].RelativeHumidity))
+		m.windspeed.With(prometheus.Labels{"location": record.Hours[0].City}).Set(record.Hours[0].WindSpeed)
+		m.pressure.With(prometheus.Labels{"location": record.Hours[0].City}).Set(record.Hours[0].PressureMSL)
+	}
 }
