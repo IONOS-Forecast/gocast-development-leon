cbf983dda95f83abff859f8de9773d2b2e715f45
Leon Zuelsdorf <leonzuelsdorf@gmail.com>
2024-10-09 08:14:09 +0200

    Testing Get with wanted Error and testing error site

--- pkg/metric/rest_test.go
+++ pkg/metric/rest_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/IONOS-Forecast/gocast-development-leon/Gocast/pkg/metric"
 )
 
+/*
 func TestGet(t *testing.T) {
 	req := httptest.NewRequest("GET", "http://localhost:8080/direct?date=2024-10-08&city=Berlin", nil)
 	rr := httptest.NewRecorder()
@@ -17,5 +18,25 @@ func TestGet(t *testing.T) {
 	//handler.ServeHTTP(rr, req)
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}*/
+
+func TestGetError(t *testing.T) {
+	req := httptest.NewRequest("GET", "http://localhost:8080/direct?date=2024-10-20&city=Berlin", nil)
+	rr := httptest.NewRecorder()
+	var h metric.Handler
+	h.Get(rr, req)
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+	}
+}
+
+func TestError(t *testing.T) {
+	req := httptest.NewRequest("GET", "http://localhost:8080/error", nil)
+	rr := httptest.NewRecorder()
+	var h metric.Handler
+	h.Get(rr, req)
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
 	}
 }
