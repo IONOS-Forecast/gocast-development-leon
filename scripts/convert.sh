ecb68b9b02cd5cfcdcf16500dfe879539650d91f
Leon Zuelsdorf <leonzuelsdorf@gmail.com>
2024-10-10 13:49:15 +0200

    Changed it so the weather_record 0 wont get converted (could conflict with database)

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -19,7 +19,7 @@ mkdir -p "../resources/pg/data"
 
 echo "generating CSV files to COPY"
 for c in "${cities[@]}"; do
-  for i in {0..7}; do
+  for i in {1..7}; do
     jq --arg city "$c" -r '.weather | del(.[].fallback_source_ids) | map([.[]]) | .[:-1] | .[] | @csv' "resources/weather_records/$c"_"$i"-orig.json >> "resources/pg/data/$c".csv
   done;
   printf "%s" "$(< resources/pg/data/$c.csv)" > "resources/pg/data/$c".csv
