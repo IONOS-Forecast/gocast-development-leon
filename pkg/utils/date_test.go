507a0a8e2f06452e09486726acddc991949f5d84
Leon Zuelsdorf <leonzuelsdorf@gmail.com>
2024-10-10 09:53:56 +0200

    Adding more tests for date.go

--- pkg/utils/date_test.go
+++ pkg/utils/date_test.go
@@ -159,3 +159,159 @@ func TestSetFutureDate(t *testing.T) {
 		t.Errorf("SetFutureDate() returned wrong date: got %v want %v", date, testDate)
 	}
 }
+
+func TestSplitDate(t *testing.T) {
+	year := 2010
+	month := 12
+	day := 5
+	expectedDate := fmt.Sprintf("%v-%.2v-%.2v", year, month, day)
+	year, month, day, err := SplitDate(expectedDate)
+	if err != nil {
+		t.Error("SplitDate() returned an error: ", err)
+	}
+	date := fmt.Sprintf("%v-%.2v-%.2v", year, month, day)
+	if expectedDate != date {
+		t.Errorf("SplitDate() returned wrong date: got %v want %v", date, expectedDate)
+	}
+	year = 2020
+	month = 5
+	day = 12
+	expectedDate = fmt.Sprintf("%v-%.2v-%.2v", year, month, day)
+	year, month, day, err = SplitDate(expectedDate)
+	if err != nil {
+		t.Error("SplitDate() returned an error: ", err)
+	}
+	date = fmt.Sprintf("%v-%.2v-%.2v", year, month, day)
+	if expectedDate != date {
+		t.Errorf("SplitDate() returned wrong date: got %v want %v", date, expectedDate)
+	}
+	now := time.Now()
+	year = now.Year()
+	month = int(now.Month())
+	day = now.Day()
+	expectedDate = fmt.Sprintf("%v-%.2v-%.2v", year, month, day)
+	year, month, day, err = SplitDate(expectedDate)
+	if err != nil {
+		t.Error("SplitDate() returned an error: ", err)
+	}
+	date = fmt.Sprintf("%v-%.2v-%.2v", year, month, day)
+	if expectedDate != date {
+		t.Errorf("SplitDate() returned wrong date: got %v want %v", date, expectedDate)
+	}
+	// Expecting errors
+	expectedDate = fmt.Sprintf("%v-%.2v-%.2v", 0, 0, 0)
+
+	yearString := "2018"
+	monthString := "5"
+	dayString := "five"
+	date = fmt.Sprintf("%v-%.2v-%.2v", yearString, monthString, dayString)
+	expectedError := fmt.Errorf("failed to convert day: strconv.Atoi: parsing \"%.2v\": invalid syntax", dayString).Error()
+	year, month, day, err = SplitDate(date)
+	if err == nil || err.Error() != expectedError {
+		t.Errorf("SplitDate() returned unexpected error: got \"%v\" want \"%v\"", err, expectedError)
+	}
+	date = fmt.Sprintf("%v-%.2v-%.2v", year, month, day)
+	if date != expectedDate {
+		t.Errorf("SplitDate() returned unexpected date: got %v want %v", date, expectedDate)
+	}
+	yearString = "2018"
+	monthString = "five"
+	dayString = "2"
+	date = fmt.Sprintf("%v-%.2v-%.2v", yearString, monthString, dayString)
+	expectedError = fmt.Errorf("failed to convert month: strconv.Atoi: parsing \"%.2v\": invalid syntax", monthString).Error()
+	year, month, day, err = SplitDate(date)
+	if err == nil || err.Error() != expectedError {
+		t.Errorf("SplitDate() returned unexpected error: got \"%v\" want \"%v\"", err, expectedError)
+	}
+	date = fmt.Sprintf("%v-%.2v-%.2v", year, month, day)
+	if date != expectedDate {
+		t.Errorf("SplitDate() returned unexpected date: got %v want %v", date, expectedDate)
+	}
+	yearString = "five"
+	monthString = "5"
+	dayString = "2"
+	date = fmt.Sprintf("%v-%.2v-%.2v", yearString, monthString, dayString)
+	expectedError = fmt.Errorf("failed to convert year: strconv.Atoi: parsing \"%v\": invalid syntax", yearString).Error()
+	year, month, day, err = SplitDate(date)
+	if err == nil || err.Error() != expectedError {
+		t.Errorf("SplitDate() returned unexpected error: got \"%v\" want \"%v\"", err, expectedError)
+	}
+	date = fmt.Sprintf("%v-%.2v-%.2v", year, month, day)
+	if date != expectedDate {
+		t.Errorf("SplitDate() returned unexpected date: got %v want %v", date, expectedDate)
+	}
+}
+
+func TestCheckDate(t *testing.T) {
+	year := 2010
+	month := 12
+	day := 5
+	expectedDate := fmt.Sprintf("%v-%.2v-%.2v", year, month, day)
+	date, err := CheckDate(expectedDate)
+	if err != nil {
+		t.Error("CheckDate() returned an error: ", err)
+	}
+	_date := date.Format("2006-01-02")
+	if expectedDate != _date {
+		t.Errorf("CheckDate() returned wrong date: got %v want %v", _date, expectedDate)
+	}
+	year = 2016
+	month = 3
+	day = 6
+	expectedDate = fmt.Sprintf("%v-%.2v-%.2v", year, month, day)
+	date, err = CheckDate(expectedDate)
+	if err != nil {
+		t.Error("CheckDate() returned an error: ", err)
+	}
+	_date = date.Format("2006-01-02")
+	if expectedDate != _date {
+		t.Errorf("CheckDate() returned wrong date: got %v want %v", _date, expectedDate)
+	}
+	now := time.Now()
+	year = now.Year()
+	month = int(now.Month())
+	day = now.Day()
+	expectedDate = fmt.Sprintf("%v-%.2v-%.2v", year, month, day)
+	date, err = CheckDate(expectedDate)
+	if err != nil {
+		t.Error("CheckDate() returned an error: ", err)
+	}
+	_date = date.Format("2006-01-02")
+	if expectedDate != _date {
+		t.Errorf("CheckDate() returned wrong date: got %v want %v", _date, expectedDate)
+	}
+	// Expecting errors
+	expectedDate = fmt.Sprintf("%v-%.2v-%.2v", year, month, day+8)
+	expectedError := fmt.Errorf("date should not be after next 7 days").Error()
+	_, err = CheckDate(expectedDate)
+	if err == nil || err.Error() != expectedError {
+		t.Errorf("CheckDate() returned unexpected error: got \"%v\" want \"%v\"", err.Error(), expectedError)
+	}
+	year = 2008
+	month = 8
+	day = 25
+	expectedDate = fmt.Sprintf("%v-%.2v-%.2v", year, month, day)
+	expectedError = fmt.Errorf("date should not be before december 2010").Error()
+	_, err = CheckDate(expectedDate)
+	if err == nil || err.Error() != expectedError {
+		t.Errorf("CheckDate() returned unexpected error: got \"%v\" want \"%v\"", err.Error(), expectedError)
+	}
+	year = 2012
+	month = 7
+	day = 32
+	expectedDate = fmt.Sprintf("%v-%.2v-%.2v", year, month, day)
+	expectedError = fmt.Errorf("parsing time \"%v\": day out of range", expectedDate).Error()
+	_, err = CheckDate(expectedDate)
+	if err == nil || err.Error() != expectedError {
+		t.Errorf("CheckDate() returned unexpected error: got \"%v\" want \"%v\"", err.Error(), expectedError)
+	}
+	year = 20240
+	month = 7
+	day = 32
+	expectedDate = fmt.Sprintf("%v-%.2v-%.2v", year, month, day)
+	expectedError = fmt.Errorf("parsing time \"%v\" as \"2006-01-02\": cannot parse \"%v\" as \"-\"", expectedDate, expectedDate[4:]).Error()
+	_, err = CheckDate(expectedDate)
+	if err == nil || err.Error() != expectedError {
+		t.Errorf("CheckDate() returned unexpected error: got \"%v\" want \"%v\"", err.Error(), expectedError)
+	}
+}
